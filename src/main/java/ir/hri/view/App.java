61c2de626c317e6192d6f43a1dfcf47e2440e967
HR <ireh.hr@gmail.com>
2016-11-14 17:03:44 +0330

    read file and save to database

--- src/main/java/ir/hri/view/App.java
+++ src/main/java/ir/hri/view/App.java
@@ -1,35 +1,51 @@
 package ir.hri.view;
 
+import ir.hri.Entity.City;
+import ir.hri.Entity.State;
 import ir.hri.Entity.User;
+import ir.hri.util.FileUtil;
 import ir.hri.util.JPAUtility;
 
 import javax.persistence.EntityManager;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        List<State> states = FileUtil.readState();
+        List<City> cities = FileUtil.readCity();
+
+        Iterator<State> stateIterator = states.iterator();
+        while (stateIterator.hasNext()) {
+            ArrayList<City> cities1 = new ArrayList<City>();
+            State state = stateIterator.next();
+
+            for (City city : cities) {
+                if (state.getId() == city.getStateId()) {
+                    cities1.add(city);
+                }
+            }
+            state.setCities(cities1);
+        }
+
+
         EntityManager entityManager = JPAUtility.getEntityManager();
         entityManager.getTransaction().begin();
-        //persist user
-        User user = new User(1, "Hamid");
-        entityManager.persist(user);
+        int i = 0;
+        for (State state : states) {
+            entityManager.persist(state);
+            if (i == 10) {
+                entityManager.getTransaction().commit();
+                entityManager.getTransaction().begin();
+                i = 0;
+            }
+            i++;
+        }
         entityManager.getTransaction().commit();
-        //refresh user
-        entityManager.refresh(user);
-        //update user
-        entityManager.getTransaction().begin();
-        user.setName("HamidReza");
-        entityManager.getTransaction().commit();
-        //remove user
-        entityManager.getTransaction().begin();
-        entityManager.remove(user);
-        entityManager.getTransaction().commit();
-        //persist user
-        entityManager.getTransaction().begin();
-        User user2 = new User(1, "HamidReza Ireh");
-        entityManager.persist(user2);
-        entityManager.getTransaction().commit();
-
         entityManager.close();
         JPAUtility.close();
     }
